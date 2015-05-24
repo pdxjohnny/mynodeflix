526777eb9e69120ee1a9b36fd10c55d651a2889d
John Andersen <johnandersenpdx@gmail.com>
2015-05-23 21:34:03 -0700

    Added auth function to api router

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+// Headers defined here
 var default_headers = {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*",
 };
-
-function set_headers (res) {
+/* Set API headers */
+router.use(function(req, res, next) {
   for (var header in default_headers)
   {
     res.setHeader(header, default_headers[header]);
   }
-}
-
-/* Set API headers */
-router.all('/', function(req, res, next) {
-  set_headers(res);
   next();
 });
+
+
+// Auth defined here
+router.use(function(req, res, next) {
+  var auth;
+  // check whether an autorization header was send    
+  if (req.headers.authorization) {
+    // only accepting basic auth, so:
+    // * cut the starting "Basic " from the header
+    // * decode the base64 encoded username:password
+    // * split the string at the colon
+    // -> should result in an array
+    auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+  }
+
+  // checks if:
+  // * auth array exists 
+  // * first value matches the expected user 
+  // * second value the expected password
+  if (!auth || auth[0] !== 'testuser' || auth[1] !== 'testpassword') {
+    // any of the tests failed
+    // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+    res.statusCode = 401;
+    // MyRealmName can be changed to anything, will be prompted to the user
+    res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+    // this will displayed in the browser when authorization is cancelled
+    res.end('Unauthorized');
+  } else {
+    // continue with processing, user was authenticated
+    next();
+  }
+});
+
+
+// Routes defined here
 
 /* GET root send OK. */
 router.get('/', function(req, res, next) {
